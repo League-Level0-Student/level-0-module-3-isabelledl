478f22f089f6e0f8799a4372814b07fa93e59647
isabelledl <league@WTS7.attlocal.net>
2018-02-07 16:55:12 -0800

    CrazyCatLady,RobotColorChooser.

--- src/elseif/CrazyCatLady.java
+++ src/elseif/CrazyCatLady.java
@@ -5,18 +5,27 @@ package elseif;
 
 import java.net.URI;
 
+import javax.swing.JOptionPane;
+
 public class CrazyCatLady {
 	public static void main(String[] args) {
 		// 1. Ask the user how many cats they have
-
-		// 2. Convert their answer into an int
-
-		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
-
-		// 4. If they have 3 or less, call the method below to show them a cat video
-
-		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
 		
+String cat = JOptionPane.showInputDialog("How many cats do you have?");
+		// 2. Convert their answer into an int
+int number =Integer.parseInt(cat);
+		// 3. If they have more than 3 cats, tell them they're a crazy cat lady
+if (number>3) {JOptionPane.showMessageDialog(null, "your a crazy cat lady!");
+	
+}
+else if (number<1) {playVideo("https://www.youtube.com/watch?v=W_juM14WHNQ");
+
+}		// 4. If they have 3 or less, call the method below to show them a cat video
+else if (number<4) {playVideo("https://www.youtube.com/watch?v=nG2rNBFzkGE");
+	
+}
+		// 5. If they say 0, show them a video of A Frog Sitting on a Bench Like a Human
+
 	}
 
 	static void playVideo(String videoURL) {
--- src/elseif/RobotColorChooser.java
+++ src/elseif/RobotColorChooser.java
@@ -5,23 +5,51 @@
 package elseif;
 
 import javax.swing.JOptionPane;
+
+import org.jointheleague.graphical.robot.Robot;
+
 import java.awt.Color;
 
 public class RobotColorChooser {
 	public static void main(String[] args) {
+		Robot sam = new Robot();
+		// 3. ask the user what color they would like the robot to d
+for (int i = 5; i < 10; i++) {
+	
 
-		//3. ask the user what color they would like the robot to draw
-		
-		//4. use an if/else statement to set the pen color that the user requested
+		String color = JOptionPane.showInputDialog("What color would you like the robot to draw in ?");
+		// 4. use an if/else statement to set the pen color that the user requested
+		if (color.equalsIgnoreCase("blue")) {
+			sam.setPenColor(0, 20, 120);
 
-        //5. if the user doesn’t enter anything, choose a random color
+		} else if (color.equalsIgnoreCase("red")) {
+			sam.setPenColor(199, 0, 20);
+		}else if (color.equalsIgnoreCase("green")) {
+			sam.setPenColor(0, 108, 20);
+		}else { JOptionPane.showMessageDialog(null, "Sorry I do have that color"); sam.setRandomPenColor();
+			
+		}
 
-        //6. put a loop around your code so that you keep asking the user for more colors & drawing them
-		
-		//2. set the pen width to 10
-		
-	    //1. make the robot draw a shape (this will take more than one line of code)
 
+		// 5. if the user doesn’t enter anything, choose a random color
 
+		// 6. put a loop around your code so that you keep asking the user for more
+		// colors & drawing them
+
+		// 2. set the pen width to 10
+		sam.setPenWidth(10);
+		// 1. make the robot draw a shape (this will take more than one line of code)
+
+		sam.penDown();
+		sam.hide();
+		sam.setSpeed(10);
+		sam.move(200);
+		sam.turn(90);
+		sam.move(200);
+		sam.turn(90);
+		sam.move(200);
+		sam.turn(90);
+		sam.move(200);
+}
 	}
 }
