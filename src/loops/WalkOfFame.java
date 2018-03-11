2ec0a6f30f6af152e84b0c9d5960e15253c31680
eliyahubowers <league@WTS20.attlocal.net>
2018-03-11 12:58:05 -0700

    WlakOfFame,FizzBuzz(IP)

--- src/loops/WalkOfFame.java
+++ src/loops/WalkOfFame.java
@@ -9,20 +9,33 @@ import org.jointheleague.graphical.robot.Robot;
 public class WalkOfFame {
 	public static void main(String[] args) {
 		Robot rob = new Robot();
-		// 1. Set the X position of the robot so that it starts on the left. 
+		// 1. Set the X position of the robot so that it starts on the left.
 		// You also need to show the robot to see the result of this line.
-rob.setX(40);
-		// 2. Make the robot draw a star shape. Hint: 144.
-rob.setSpeed(10);rob.penDown();
-for (int i = 1 ; i < 6; i++) {rob.turn(144);rob.move(144);
-	
-}
+		rob.setX(600);
+		rob.setSpeed(10);
+		rob.penDown();
+		rob.setPenWidth(10);
+		rob.hide();
+		for (int i = 1; i < 6; i++) {
+
+			// 2. Make the robot draw a star shape. Hint: 144.
+
+			for (int i1 = 1; i1 < 6; i1++) {
+				rob.turn(144);
+				rob.move(144);
+
+			}
+			rob.turn(-90);
+			rob.penUp();
+			rob.move(200);
+			rob.turn(90);
+rob.penDown();
+		}
 		// 3. Set the size of the star to 30.
-rob.
+
 		/** THE CHALLENGE: **/
 		/*
-		 * Make the robot draw a line of stars like this:
-		 * http://bit.ly/walk-of-fame
+		 * Make the robot draw a line of stars like this: http://bit.ly/walk-of-fame
 		 * 
 		 * Hint: The distance between stars is 50.
 		 */
