d9c1329c939ef90a60a7955053cc84704d2b708a
ICanSayNo <league@WorthyMini-5.attlocal.net>
2018-03-21 16:59:39 -0700

    Obedient(IP)

---
+++ src/modulo/Obedient.java
@@ -0,0 +1,39 @@
+package modulo;
+
+import org.jointheleague.graphical.robot.Robot;
+
+public class Obedient {
+	Robot art = new Robot();
+
+	public static void main(String[] args) {
+		Obedient cat =new Obedient();
+//cat.drawStar();
+cat.drawShape(300);
+	}
+
+	void drawStar() {
+		for (int i = 1; i < 100; i++) {
+
+			art.penDown();
+			art.hide();
+			art.setSpeed(100);
+			art.move(100);
+			art.turn(90);
+			art.move(100);
+			art.turn(144);
+
+		}
+
+	}
+	void drawShape(int sides) {
+		
+		art.penDown();	
+		art.hide();
+		art.setSpeed(100);
+		for (int i = 0; i < sides; i++) {
+			art.move(500/sides);
+			art.turn(360/sides);
+		}
+	}
+	}
+
--- src/modulo/Robot.java
+++
@@ -1,5 +0,0 @@
-package modulo;
-
-public class Robot {
-
-}
